ace6a08edf443c4e6c8e29ccd36e2a39abdfbfdb
annikaverma <76493450+annikaverma@users.noreply.github.com>
2021-05-09 18:26:25 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
+
 function setup() {
-    canvas = createCanvas(300, 300);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.hide();
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/model.json',modelLoaded);
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
+
+function gotResult(erro, results) {
+  if (error) {
+    console.erroe(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_objects_name").innerHTML = results[0].label;
+    document.getElementById("result_objects_accuracy").innerHTML = results[0].confidence.toFixed(3);
   }
-  
-  function modelLoaded() {
-      console.log('Model Loaded!');
-  }
-  
-  function draw() {
-    image(video, 0, 0, 300, 300);
-    classifier.classify(video, gotResult);
-  }
-  
-  function gotResult(error, results) {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(results);
-      document.getElementById("result_object_name").innerHTML = results[0].label;
-      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-    }
-  }
-  
+}
